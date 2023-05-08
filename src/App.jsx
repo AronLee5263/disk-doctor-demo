6a0f095f44be3d88dd7cc3c40c0b32bed9640bee
Aron <aronlee5263@gmail.com>
2023-05-08 19:07:48 +0900

    파일 위치 변경

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         </a>
       </div>
       {/* <h1>Vite + React</h1> */}
-      <h1>시작해보자 disk-doctor</h1>
+      <h1> disk-doctor</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
